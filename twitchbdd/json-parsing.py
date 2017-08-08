c5f0c89fa5208fcb5953094e1b5328e46c9d7b79
Maxime Favier <maxime1.favier@free.fr>
2017-08-08 17:49:27 +0200

    v.0.0.4 (unfinished)

--- twitchbdd/json-parsing.py
+++ twitchbdd/json-parsing.py
@@ -3,18 +3,17 @@ import json
 with open('download/file1.json', 'r') as f:
     data = json.load(f)
 
-# print(data)
-keys = data.keys()
-print(keys)
-
 chatters = data['chatters']
-print(chatters)
+
 
 viewers = chatters['viewers']
-print(viewers)
 global_mods = chatters['global_mods']
-print(global_mods)
 admins = chatters['admins']
-print(admins)
 moderators = chatters['moderators']
-print(moderators)
+
+viewers.extend(global_mods)
+viewers.extend(admins)
+viewers.extend(moderators)
+
+if debug == 1:
+
