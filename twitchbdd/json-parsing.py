5049d884083478ff5750bf000d9e666176e09689
Maxime Favier <maxime1.favier@free.fr>
2017-08-08 18:53:52 +0200

    v.0.0.5 (unfinished)

--- twitchbdd/json-parsing.py
+++ twitchbdd/json-parsing.py
@@ -1,16 +1,27 @@
-import json
+try:
+    import json
 
-with open('download/file1.json', 'r') as f:
+except ImportError:
+    raise ImportError('unable to find json package')
+
+# open and load data in dictionary
+try:
+    with open('download/file1.json', 'r') as f:
     data = json.load(f)
 
+except FileNotFoundError:
+    raise FileNotFoundError('unable to find the downloaded file')
+
+# recover the list of viewers
 chatters = data['chatters']
 
-
+# recover the list of viewer and all type of moderator
 viewers = chatters['viewers']
 global_mods = chatters['global_mods']
 admins = chatters['admins']
 moderators = chatters['moderators']
 
+# add all type of moderator in the viewer list
 viewers.extend(global_mods)
 viewers.extend(admins)
 viewers.extend(moderators)
