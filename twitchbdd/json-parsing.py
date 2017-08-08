0273446334a38ee3723b7edcd59dbc5e4757a8ac
Maxime Favier <maxime1.favier@free.fr>
2017-08-08 14:52:50 +0200

    v.0.0.3 (unfinished)

--- twitchbdd/json-parsing.py
+++ twitchbdd/json-parsing.py
@@ -2,3 +2,13 @@ import json
 
 with open('download/file1.json', 'r') as f:
     data = json.load(f)
+
+# print(data)
+keys = data.keys()
+print(keys)
+
+chatters = data['chatters']
+print(chatters)
+
+viewers = chatters['viewers']
+print(viewers)
