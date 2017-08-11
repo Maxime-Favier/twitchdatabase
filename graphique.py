9455d34b401b1f9adb091882ec3362604d33eab6
Maxime Favier <maxime1.favier@free.fr>
2017-08-11 16:22:42 +0200

    v 0.1.1 (unfinished)

--- graphique.py
+++ graphique.py
@@ -15,9 +15,9 @@ name = StringVar()
 
 tutorial = Label(window, text="twitch username:", font=("Helvetica", 12))
 name = Entry(window, textvariable=name, width=20)
-line_break = Label(window, text= " ", font=("Helvetica", 16))
-stop = Button(text="quit", command=quit, font=("Helvetica", 10))
-ok = Button(text="Ok", command=getname, font=("Helvetica", 16))
+line_break = Label(window, text= " ", font=("Helvetica", 11))
+stop = Button(text="quit", command=quit, font=("Helvetica", 11))
+ok = Button(text="Ok", command=getname, font=("Helvetica", 11))
 
 tutorial.grid(row=0, column=1)
 name.grid(row=0, column=2, columnspan=2)
