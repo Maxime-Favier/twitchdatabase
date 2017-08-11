b6a68968c07f5c4102d50a001783495418002357
Maxime Favier <maxime1.favier@free.fr>
2017-08-11 17:26:48 +0200

    v 0.1.1 (unfinished)

--- graphique.py
+++ graphique.py
@@ -13,17 +13,21 @@ pseudo = ""
 name = StringVar()
 
 
-tutorial = Label(window, text="twitch username:", font=("Helvetica", 12))
+twitchdatabase = Label(window, text= "TwitchDatabase", font=("Helvetica", 14))
+line_break_1 = Label(window, text= " ", font=("Helvetica", 11))
+tutorial = Label(window, text="twitch username:", font=("Helvetica", 11))
 name = Entry(window, textvariable=name, width=20)
-line_break = Label(window, text= " ", font=("Helvetica", 11))
+line_break_2 = Label(window, text= " ", font=("Helvetica", 11))
 stop = Button(text="quit", command=quit, font=("Helvetica", 11))
 ok = Button(text="Ok", command=getname, font=("Helvetica", 11))
 
-tutorial.grid(row=0, column=1)
-name.grid(row=0, column=2, columnspan=2)
-line_break.grid(row=2)
-stop.grid(row=3, column=1)
-ok.grid(row=3, column=3)
+twitchdatabase.grid(row=1, columnspan=5)
+line_break_1.grid(row=2)
+tutorial.grid(row=3, column=1)
+name.grid(row=3, column=2, columnspan=2)
+line_break_2.grid(row=4)
+stop.grid(row=5, column=1)
+ok.grid(row=5, column=3)
 
 
 window.mainloop()
