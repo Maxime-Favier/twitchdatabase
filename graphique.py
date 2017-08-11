9f9aca193b7cd8e602cbe19ebf0763afbbdf3f90
Maxime Favier <maxime1.favier@free.fr>
2017-08-11 18:03:30 +0200

    v 0.1.1 (unfinished)

--- graphique.py
+++ graphique.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import Img
 
 
 def getname():
@@ -9,22 +10,23 @@ def getname():
 
 
 window = Tk()
+window.wm_title("Twitch database")
 pseudo = ""
 name = StringVar()
 
 
-twitchdatabase = Label(window, text= "TwitchDatabase", font=("Helvetica", 14))
-line_break_1 = Label(window, text= " ", font=("Helvetica", 11))
+twitch_database = Label(window, text="TwitchDatabase", font=("Helvetica", 14))
+line_break_1 = Label(window, text=" ", font=("Helvetica", 10))
 tutorial = Label(window, text="twitch username:", font=("Helvetica", 11))
 name = Entry(window, textvariable=name, width=20)
-line_break_2 = Label(window, text= " ", font=("Helvetica", 11))
+line_break_2 = Label(window, text=" ", font=("Helvetica", 11))
 stop = Button(text="quit", command=quit, font=("Helvetica", 11))
 ok = Button(text="Ok", command=getname, font=("Helvetica", 11))
 
-twitchdatabase.grid(row=1, columnspan=5)
+twitch_database.grid(row=1, columnspan=5)
 line_break_1.grid(row=2)
-tutorial.grid(row=3, column=1)
-name.grid(row=3, column=2, columnspan=2)
+tutorial.grid(row=3, column=1, sticky=W)
+name.grid(row=3, column=2, columnspan=2, padx=10, sticky=W)
 line_break_2.grid(row=4)
 stop.grid(row=5, column=1)
 ok.grid(row=5, column=3)
