3f257847088f0d73592f192b1837b9d4de241c33
Maxime Favier <maxime1.favier@free.fr>
2017-08-11 14:39:35 +0200

    v 0.1.1 (unfinished)

--- graphique.py
+++ graphique.py
@@ -2,23 +2,31 @@ from tkinter import *
 
 
 def getname():
+    global pseudo
     pseudo = name.get()
-    print(pseudo)
+    window.destroy()
+    # print(pseudo)
 
 
 window = Tk()
-
+pseudo = ""
 name = StringVar()
 
-tutorial = Label(window, text="twitch username")
+
+tutorial = Label(window, text="twitch username:", font=("Helvetica", 12))
 name = Entry(window, textvariable=name, width=20)
-stop = Button(text="quit", command=quit)
-ok = Button(text="Ok", command=getname)
+line_break = Label(window, text= " ", font=("Helvetica", 16))
+stop = Button(text="quit", command=quit, font=("Helvetica", 10))
+ok = Button(text="Ok", command=getname, font=("Helvetica", 16))
 
 tutorial.grid(row=0, column=1)
 name.grid(row=0, column=2, columnspan=2)
-stop.grid(row=2, column=1)
-ok.grid(row=2, column=3)
+line_break.grid(row=2)
+stop.grid(row=3, column=1)
+ok.grid(row=3, column=3)
 
 
 window.mainloop()
+print(pseudo)
+
+
