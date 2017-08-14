9cc0e63215cbf3fa2d7056429cdbc49057bb635b
Maxime-le-Goupil <maxime1.favier@free.fr>
2017-08-14 19:03:31 +0200

    0.1.3 (unfinished)

--- graphique.py
+++ graphique.py
@@ -1,5 +1,6 @@
 from tkinter import *
-import Img
+import PIL
+
 
 # get name function
 def getname():
@@ -17,12 +18,13 @@ name = StringVar()
 
 
 twitch_database = Label(window, text="TwitchDatabase", font=("Helvetica", 14))
-line_break_1 = Label(window, text=" ", font=("Helvetica", 10))
+line_break_1 = Label(window, text=" ", font=("Helvetica", 5))
 tutorial = Label(window, text="twitch username:", font=("Helvetica", 11))
 name = Entry(window, textvariable=name, width=20)
 line_break_2 = Label(window, text=" ", font=("Helvetica", 11))
 stop = Button(text="quit", command=quit, font=("Helvetica", 11))
 ok = Button(text="Ok", command=getname, font=("Helvetica", 11))
+line_break_3 = Label(window, text=" ", font=("Helvetica", 5))
 
 twitch_database.grid(row=1, columnspan=5)
 line_break_1.grid(row=2)
@@ -31,6 +33,7 @@ name.grid(row=3, column=2, columnspan=2, padx=10, sticky=W)
 line_break_2.grid(row=4)
 stop.grid(row=5, column=1)
 ok.grid(row=5, column=3)
+line_break_3.grid(row=6)
 
 
 window.mainloop()
